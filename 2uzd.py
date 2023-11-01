e6f9fa2df071429cd21b6a53cb6e47d407cb7476
KarlisKreps <karlis.kreps@skolo.lv>
2023-11-01 17:28:37 +0200

    uzd

--- 2uzd.py
+++ 2uzd.py
@@ -3,3 +3,10 @@ Iegūt ziņas, izmantojot rss no google.lv.
 
 """
 
+import feedparser
+rss_url = 'https://news.google.com/rss?hl=en-LV&gl=LV&ceid=LV:en'
+feed=feedparser.parse(rss_url)
+for entry in feed.entries:
+    print("Virsraksts:", entry.title)
+    print("Saite:", entry.link)
+    print("\n")
