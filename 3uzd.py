5aabf825d93ff1fcfd0ecf5bb69a0bd7f8277c1a
KarlisKreps <karlis.kreps@skolo.lv>
2023-11-01 17:38:55 +0200

    uzd

--- 3uzd.py
+++ 3uzd.py
@@ -5,3 +5,10 @@ piemēram, vārdu, vecumu un ceļojumam iemaksāto summu.
 
 """
 
+class Dati:
+    def __init__(self, vards,vecums,iemaksata_summa):
+        self.vards = vards
+        self.vecums = vecums
+        self.iemaksata_summa = iemaksata_summa
+
+mani_dati = Dati("Janis", 30, 500)
