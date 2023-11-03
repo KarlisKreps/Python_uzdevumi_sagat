add844440ed7dd96893a9c1f39ec19b3e58d3118
KarlisKreps <karlis.kreps@skolo.lv>
2023-11-03 15:52:20 +0200

    uzd

--- 11uzd.py
+++ 11uzd.py
@@ -3,3 +3,14 @@ Uzrakstiet Python programmu, lai trīskāršotu visus dotā veselo skaitļu sara
 Izmantojiet Python map() funkciju.
 """
 
+skaitlu_saraksts=[1,2,3,4,5,6,7,8]
+reizinajums=[]
+
+def reizinat(cipars):
+    if isinstance(cipars, int):
+        return cipars * 3
+    else:
+        return cipars
+
+reizinajums = list(map(reizinat,skaitlu_saraksts))
+print(reizinajums)
