3052a90108cb465918f19f405e79b6461158aae3
KarlisKreps <karlis.kreps@skolo.lv>
2023-11-03 15:42:04 +0200

    uzd

--- 9uzd.py
+++ 9uzd.py
@@ -4,3 +4,19 @@ katram saraksta(virknes) loceklim.
 
 """
 
+
+skaitli=[1,3,5,67,44,25]
+reizinajums=[]
+
+def reizinat(reizinatajs):
+    if reizinatajs > 0 and reizinatajs < 5:
+        return 2 * reizinatajs
+
+    if reizinatajs > 4:
+        return 4 * reizinatajs
+    
+    if reizinatajs > 40:
+        return 20 * reizinatajs
+
+reizinajums = list(map(reizinat,skaitli))
+print(reizinajums)
