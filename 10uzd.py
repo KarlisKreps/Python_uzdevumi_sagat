231b2f8c8d6c29761ff058a79600b901c53d987a
KarlisKreps <karlis.kreps@skolo.lv>
2023-11-03 15:47:00 +0200

    uzd

--- 10uzd.py
+++ 10uzd.py
@@ -3,3 +3,10 @@ Izmantojot map() funkciju izveidot no diviem sarakstiem saliktnei.
 
 
 """
+
+klases = ['a', 'b', 'c', 'd']
+skaitli = [1,2,3,4]
+
+saliktenis = list(map(lambda x,y: str(x) + y, skaitli, klases))
+
+print(saliktenis)
