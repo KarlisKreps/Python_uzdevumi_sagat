d9b1e1242c7fbce47cabea0f4158aac394570470
KarlisKreps <karlis.kreps@skolo.lv>
2023-11-03 15:26:31 +0200

    uzfd

--- 6uzd.py
+++ 6uzd.py
@@ -2,11 +2,11 @@
 #Papildināt doto, izveidojot atbilstošu klasi!.
 
 
-
-
-
-
-
+class APRAKSTS:
+    def __init__(self, nosaukums, gads, autors):
+        self.nosaukums = nosaukums
+        self.gads = gads
+        self.autors = autors
 
 kt2022=APRAKSTS("Vasara", 2015, "Kate Ziemele")
 print(kt2022.nosaukums, kt2022.gads, kt2022.autors)
