9d414ab76a1e6364222c8a3fd3eb666c76458792
KarlisKreps <karlis.kreps@skolo.lv>
2023-11-03 16:11:37 +0200

    uzd

--- 12uzd.py
+++ 12uzd.py
@@ -3,3 +3,11 @@ Uzrakstiet Python programmu, lai diviem sarakstiem noteikti atšķirību.
 Izmantojiet funkciju map().
 """
 
+saraksts1 = [1,2,3,4,5]
+saraksts2 = [3,4,5,6,7]
+
+def atslega(skaitlis1, skaitlis2):
+    return skaitlis1-skaitlis2
+
+atskiribas_saraksts = list(map(atslega, saraksts1,saraksts2))
+print(atskiribas_saraksts)
--- 13uzd.py
+++ 13uzd.py
@@ -3,3 +3,18 @@ Doti divi saraksti.
 Izmantojot funkciju map(), izdrukāt abu sarakstu vienādo( sakrīt elementi un to index) elementu summu.
 """
 
+saraksts1 = [1,2,3,4,5]
+saraksts2 = [1,2,5,4,7]
+
+def saskaitit_elementus(skaitlis1, skaitlis2, indekss):
+    if skaitlis1 == skaitlis2 and saraksts1.index(skaitlis1) == indekss:
+        return skaitlis1 + skaitlis2
+    else:
+        return 0 
+    
+sasummetie_elementi = list(map(saskaitit_elementus, saraksts1, saraksts2, range(len(saraksts1))))
+summa = sum(sasummetie_elementi)
+print("Vienado elementu summa:", summa)
+                           
+    
+    
