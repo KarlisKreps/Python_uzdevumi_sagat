c897cb598365f9fdfd983e879d8e680002048686
KarlisKreps <karlis.kreps@skolo.lv>
2023-11-03 15:34:38 +0200

    uzdf

--- 7uzd.py
+++ 7uzd.py
@@ -4,5 +4,13 @@ pievienots klāt doktora nosaukums - Dr.
 """
 
 
+
+
 saraksts1=["Kalniņš", "Opmanis", "Vēzis", "Almane"]
-sarakts2=[]
+saraksts2=[]
+
+def pievienot_doktora_nosaukumu(uzvards):
+    return "Dr. " + uzvards
+
+saraksts2 = list(map(pievienot_doktora_nosaukumu,saraksts1))
+print(saraksts2)
